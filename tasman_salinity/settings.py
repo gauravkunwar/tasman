bc2a7e57222d441bc8cf339b2cdef60eb9c3cc35
Gaurav Kunwar <gauravkunwar@aol.com>
2021-04-07 17:43:14 +1000

    gitignore & security

--- tasman_salinity/settings.py
+++ tasman_salinity/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+from .secrets import *
 
 from pathlib import Path
 
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!_zv*l+r1nacauhzna8-5uamu$g!889_0t-d=jdu^g%y$ao9@e'
+# imported from 'secrets.py'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
